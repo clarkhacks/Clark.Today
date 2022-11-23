9e5df3d5a19f09a8e3039d4f4f27a8babe601964
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-11-23 11:37:21 -0600

    Domain

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Clark Weckmann',
-  domain: 'dev.blog.clark.today',
+  domain: 'blog.clark.today',
   author: 'Clark Weckmann',
 
   // open graph metadata (optional)
